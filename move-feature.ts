732c0b3ef49ffe78fe2708a025be78cbb5ba7750
Mahendra Kumar <mahendra.kumar@rabobank.nl>
2023-02-03 10:51:17 +0100

    moved functions

--- move-feature.ts
+++ move-feature.ts
@@ -3,12 +3,16 @@ import { Arguments, Argv } from "yargs";
 import fs from "fs-extra";
 import path from "path";
 import {
-  updateExtendsInTsConfig,
-  updateEsLintrc,
-  updateJestConfig,
-  updateOutDirPath,
-  updateProjectJson,
-  updateFeaturePath,
+  // updateExtendsInTsConfig,
+  // updateEsLintrc,
+  // updateJestConfig,
+  // updateOutDirPath,
+  // updateProjectJson,
+  // updateFeaturePath,
+  modifyPropertyInJsonFile,
+  updateJSONFile,
+  updatePreset
+
 } from "./utils";
 
 export interface MoveFeatureArgs extends Arguments {
@@ -72,4 +76,88 @@ async function moveDirectory(src, dest) {
   } catch (err) {
     console.error(err);
   }
+}
+
+async function updateExtendsInTsConfig(dest) {
+  const pathToBaseConfig = path.join(path.resolve(), "tsconfig.base.json");
+  const relativePathToBaseConfig = path.relative(dest, pathToBaseConfig);
+
+  const destinationCofigFile = path.join(dest, "tsconfig.json");
+  await modifyPropertyInJsonFile(
+    destinationCofigFile,
+    "extends",
+    relativePathToBaseConfig
+  );
+}
+
+async function updateEsLintrc(dest) {
+  const isFeatureInInvestment = dest.split(path.sep).includes("investments");
+  const currentEslintrcPath = isFeatureInInvestment
+    ? path.join(
+        path.resolve(),
+        "libs/features/investments/config/.eslintrc.json"
+      )
+    : path.join(path.resolve(), ".eslintrc.json");
+  const newRelativeEsLintrcPath = path.relative(dest, currentEslintrcPath);
+  const destinationEsLintrcPath = path.join(
+    path.resolve(dest),
+    ".eslintrc.json"
+  );
+  await modifyPropertyInJsonFile(destinationEsLintrcPath, "extends", [
+    newRelativeEsLintrcPath,
+  ]);
+}
+
+async function updateJestConfig(dest) {
+  const baseJestConfigPath = path.join(path.resolve(), "jest.preset.js");
+  const newRelativeJestConfigPath = path.relative(dest, baseJestConfigPath);
+  const newFilePath = path.join(path.resolve(dest), "jest.config.ts");
+  await updatePreset(newFilePath, newRelativeJestConfigPath);
+}
+
+export async function updateOutDirPath(dest) {
+  const pathOfOutDir = path.join(path.resolve(), "dist", "out-tsc");
+  const relativePathOfOutDir = path.relative(dest, pathOfOutDir);
+  const destPathFromRoot = dest.substring(dest.indexOf("/libs"));
+  const updatedPathForConfg = path.join(relativePathOfOutDir, destPathFromRoot);
+
+  const tsConfigLibPath = path.join(path.resolve(dest), "tsconfig.lib.json");
+  const tsConfigSpecPath = path.join(path.resolve(dest), "tsconfig.spec.json");
+  await modifyPropertyInJsonFile(
+    tsConfigLibPath,
+    "compilerOptions.outDir",
+    updatedPathForConfg
+  );
+  await modifyPropertyInJsonFile(
+    tsConfigSpecPath,
+    "compilerOptions.outDir",
+    updatedPathForConfg
+  );
+}
+
+export async function updateProjectJson(src, dest) {
+  const baseProjectSchemaPath = path.resolve(
+    "node_modules/nx/schemas/project-schema.json"
+  );
+  const relativeBaseProjectSchemaPath = path.relative(
+    dest,
+    baseProjectSchemaPath
+  );
+  const destFilePath = path.join(path.resolve(dest), "project.json");
+
+  // update $schema with value with path relative to destination
+  await modifyPropertyInJsonFile(
+    destFilePath,
+    '$schema',
+    relativeBaseProjectSchemaPath
+  );
+
+  updateFeaturePath(destFilePath,src, dest);
+}
+
+export async function updateFeaturePath(filePath, src, dest){
+  const find = src.substring(src.indexOf("libs/features"));
+  const replace = dest.substring(dest.indexOf("libs/features"));
+  await updateJSONFile(filePath, find, replace);
+  console.log(`The ${path.basename(filePath)} has been updated!`);
 }
--- utils/index.ts
+++ utils/index.ts
@@ -1,91 +1,91 @@
 import fs from "fs-extra";
 import path from "path";
 
-export async function updateExtendsInTsConfig(dest) {
-  const pathToBaseConfig = path.join(path.resolve(), "tsconfig.base.json");
-  const relativePathToBaseConfig = path.relative(dest, pathToBaseConfig);
+// export async function updateExtendsInTsConfig(dest) {
+//   const pathToBaseConfig = path.join(path.resolve(), "tsconfig.base.json");
+//   const relativePathToBaseConfig = path.relative(dest, pathToBaseConfig);
 
-  const destinationCofigFile = path.join(dest, "tsconfig.json");
-  await modifyPropertyInJsonFile(
-    destinationCofigFile,
-    "extends",
-    relativePathToBaseConfig
-  );
-}
+//   const destinationCofigFile = path.join(dest, "tsconfig.json");
+//   await modifyPropertyInJsonFile(
+//     destinationCofigFile,
+//     "extends",
+//     relativePathToBaseConfig
+//   );
+// }
 
-export async function updateEsLintrc(dest) {
-  const isFeatureInInvestment = dest.split(path.sep).includes("investments");
-  const currentEslintrcPath = isFeatureInInvestment
-    ? path.join(
-        path.resolve(),
-        "libs/features/investments/config/.eslintrc.json"
-      )
-    : path.join(path.resolve(), ".eslintrc.json");
-  const newRelativeEsLintrcPath = path.relative(dest, currentEslintrcPath);
-  const destinationEsLintrcPath = path.join(
-    path.resolve(dest),
-    ".eslintrc.json"
-  );
-  await modifyPropertyInJsonFile(destinationEsLintrcPath, "extends", [
-    newRelativeEsLintrcPath,
-  ]);
-}
+// export async function updateEsLintrc(dest) {
+//   const isFeatureInInvestment = dest.split(path.sep).includes("investments");
+//   const currentEslintrcPath = isFeatureInInvestment
+//     ? path.join(
+//         path.resolve(),
+//         "libs/features/investments/config/.eslintrc.json"
+//       )
+//     : path.join(path.resolve(), ".eslintrc.json");
+//   const newRelativeEsLintrcPath = path.relative(dest, currentEslintrcPath);
+//   const destinationEsLintrcPath = path.join(
+//     path.resolve(dest),
+//     ".eslintrc.json"
+//   );
+//   await modifyPropertyInJsonFile(destinationEsLintrcPath, "extends", [
+//     newRelativeEsLintrcPath,
+//   ]);
+// }
 
-export async function updateJestConfig(dest) {
-  const baseJestConfigPath = path.join(path.resolve(), "jest.preset.js");
-  const newRelativeJestConfigPath = path.relative(dest, baseJestConfigPath);
-  const newFilePath = path.join(path.resolve(dest), "jest.config.ts");
-  await updatePreset(newFilePath, newRelativeJestConfigPath);
-}
+// export async function updateJestConfig(dest) {
+//   const baseJestConfigPath = path.join(path.resolve(), "jest.preset.js");
+//   const newRelativeJestConfigPath = path.relative(dest, baseJestConfigPath);
+//   const newFilePath = path.join(path.resolve(dest), "jest.config.ts");
+//   await updatePreset(newFilePath, newRelativeJestConfigPath);
+// }
 
-export async function updateOutDirPath(dest) {
-  const pathOfOutDir = path.join(path.resolve(), "dist", "out-tsc");
-  const relativePathOfOutDir = path.relative(dest, pathOfOutDir);
-  const destPathFromRoot = dest.substring(dest.indexOf("/libs"));
-  const updatedPathForConfg = path.join(relativePathOfOutDir, destPathFromRoot);
+// export async function updateOutDirPath(dest) {
+//   const pathOfOutDir = path.join(path.resolve(), "dist", "out-tsc");
+//   const relativePathOfOutDir = path.relative(dest, pathOfOutDir);
+//   const destPathFromRoot = dest.substring(dest.indexOf("/libs"));
+//   const updatedPathForConfg = path.join(relativePathOfOutDir, destPathFromRoot);
 
-  const tsConfigLibPath = path.join(path.resolve(dest), "tsconfig.lib.json");
-  const tsConfigSpecPath = path.join(path.resolve(dest), "tsconfig.spec.json");
-  await modifyPropertyInJsonFile(
-    tsConfigLibPath,
-    "compilerOptions.outDir",
-    updatedPathForConfg
-  );
-  await modifyPropertyInJsonFile(
-    tsConfigSpecPath,
-    "compilerOptions.outDir",
-    updatedPathForConfg
-  );
-}
+//   const tsConfigLibPath = path.join(path.resolve(dest), "tsconfig.lib.json");
+//   const tsConfigSpecPath = path.join(path.resolve(dest), "tsconfig.spec.json");
+//   await modifyPropertyInJsonFile(
+//     tsConfigLibPath,
+//     "compilerOptions.outDir",
+//     updatedPathForConfg
+//   );
+//   await modifyPropertyInJsonFile(
+//     tsConfigSpecPath,
+//     "compilerOptions.outDir",
+//     updatedPathForConfg
+//   );
+// }
 
-export async function updateProjectJson(src, dest) {
-  const baseProjectSchemaPath = path.resolve(
-    "node_modules/nx/schemas/project-schema.json"
-  );
-  const relativeBaseProjectSchemaPath = path.relative(
-    dest,
-    baseProjectSchemaPath
-  );
-  const destFilePath = path.join(path.resolve(dest), "project.json");
+// export async function updateProjectJson(src, dest) {
+//   const baseProjectSchemaPath = path.resolve(
+//     "node_modules/nx/schemas/project-schema.json"
+//   );
+//   const relativeBaseProjectSchemaPath = path.relative(
+//     dest,
+//     baseProjectSchemaPath
+//   );
+//   const destFilePath = path.join(path.resolve(dest), "project.json");
 
-  // update $schema with value with path relative to destination
-  await modifyPropertyInJsonFile(
-    destFilePath,
-    '$schema',
-    relativeBaseProjectSchemaPath
-  );
+//   // update $schema with value with path relative to destination
+//   await modifyPropertyInJsonFile(
+//     destFilePath,
+//     '$schema',
+//     relativeBaseProjectSchemaPath
+//   );
 
-  updateFeaturePath(destFilePath,src, dest);
-}
+//   updateFeaturePath(destFilePath,src, dest);
+// }
 
-export async function updateFeaturePath(filePath, src, dest){
-  const find = src.substring(src.indexOf("libs/features"));
-  const replace = dest.substring(dest.indexOf("libs/features"));
-  await updateJSONFile(filePath, find, replace);
-  console.log(`The ${path.basename(filePath)} has been updated!`);
-}
+// export async function updateFeaturePath(filePath, src, dest){
+//   const find = src.substring(src.indexOf("libs/features"));
+//   const replace = dest.substring(dest.indexOf("libs/features"));
+//   await updateJSONFile(filePath, find, replace);
+//   console.log(`The ${path.basename(filePath)} has been updated!`);
+// }
 
-async function updatePreset(filePath: string, newPreset: string): Promise<void> {
+export async function updatePreset(filePath: string, newPreset: string): Promise<void> {
   try {
     let data = await fs.readFile(filePath, "utf8");
     data = data.replace(/preset:.*,/g, `preset: '${newPreset}',`);
@@ -98,7 +98,7 @@ async function updatePreset(filePath: string, newPreset: string): Promise<void> 
   }
 }
 
-async function modifyPropertyInJsonFile(
+export async function modifyPropertyInJsonFile(
   filePath: string,
   key: string,
   newValue: string | Array<string>
@@ -127,7 +127,7 @@ async function modifyPropertyInJsonFile(
   }
 }
 
-const replaceStringInObject = async (obj, find, replace) => {
+export const replaceStringInObject = async (obj, find, replace) => {
   for (const key in obj) {
     if (typeof obj[key] === "string") {
       obj[key] = obj[key].replace(find, replace);
@@ -137,7 +137,7 @@ const replaceStringInObject = async (obj, find, replace) => {
   }
 };
 
-const updateJSONFile = async (filePath, find, replace): Promise<void> => {
+export const updateJSONFile = async (filePath, find, replace): Promise<void> => {
   try {
     const data = await fs.readJson(filePath);
     await replaceStringInObject(data, find, replace);
